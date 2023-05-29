39c8e4f5d1648c5316721930320b32ab53b41caf
hungnguyenmanhGL <nguyenmanhhung.vnu.uet@gmail.com>
2023-05-29 11:51:38 +0700

    Add sfx setting in pause panel

--- Assets/Game/Scripts/Controller/AudioManager.cs
+++ Assets/Game/Scripts/Controller/AudioManager.cs
@@ -6,7 +6,16 @@ public class AudioManager : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
 
+    private bool sfxOn = true;
+
     public void Play(AudioClip requestedAudio) {
         audioSource.PlayOneShot(requestedAudio);
+    }
+
+    public void ToggleSfx() {
+        sfxOn = !sfxOn;
+        //Debug.Log(string.Format("SFX {0}", sfxOn));
+        if (sfxOn) audioSource.volume = 1;
+        else audioSource.volume = 0;
     }
 }
--- Assets/Game/Scripts/UI/PausePanel.cs
+++ Assets/Game/Scripts/UI/PausePanel.cs
@@ -2,18 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PausePanel : Panel
 {
+    [Header("Sfx")]
+    [SerializeField] private Toggle sfxToggle;
+    [SerializeField] private TextMeshProUGUI sfxToggleTxt;
+
+    [Header("Buttons")]
     [SerializeField] private Button replayBtn;
     [SerializeField] private Button homeBtn;
 
     public delegate void DelPanelStateChanged();
     public static event DelPanelStateChanged OnPanelStateChange;
+
+    private const string toggleOff = "Off";
+    private const string toggleOn = "On";
+
     public override void Init() {
         base.Init();
+
+        sfxToggle.onValueChanged.AddListener(delegate { OnSfxToggle(); });
+
         replayBtn.onClick.AddListener(OnReplayBtnClicked);
         homeBtn.onClick.AddListener(OnHomeBtnClicked);
+    }
+
+    public void OnSfxToggle() {
+        if (!sfxToggle.isOn) sfxToggleTxt.text = toggleOff;
+        else sfxToggleTxt.text = toggleOn;
+
+        GameController.instance.AudioManager.ToggleSfx();
     }
 
     public void OnReplayBtnClicked() {
